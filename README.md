7bf993490fda01da41a9dda2bd6c44f3b3dd7b3c
Amit Ambardekar <amitamb@gmail.com>
2013-06-07 05:53:01 +0530

    Laid out initial plan

--- README.md
+++ README.md
@@ -2,5 +2,3 @@ screen.js
 =========
 
 Client side screen recording library
-
-ruby -rsocket -e 's=TCPServer.new(5**5);loop{_=s.accept;_<<"HTTP/1.0 200 OK\r\n\r\n#{File.read(_.gets.split[1])rescue nil}";_.close}'
--- screen.js
+++ screen.js
@@ -1,28 +1,71 @@
 // screen.js
 
 (function($){
-  $(function(){
 
-    
-    function getPageHTML(){
-      var re = /<script\b[^>]*>([\s\S]*?)<\/script>/gm;
-      var html = $("html").html().replace(re, "");
-      if ( html.indexOf("<head>") >= 0 ) {
-        html = html.replace("<head>", "<head><base href='"+ window.location.href +"' />");
-      }
-      return html;
+  function getPageHTMLWithCSS(){
+    var re = /<script\b[^>]*>([\s\S]*?)<\/script>/gm;
+    var htmlElem = $("html").clone();
+    htmlElem.find("script").remove();
+
+    // TODO: Convert stylesheets to inline css or
+    // make sure URL changes happening in future doesn't
+    // botch up the page
+
+    // TODO: Also convert images to base64 encoded
+    // string so as that is also not an external dependancy
+
+    return htmlElem.html();
+  };
+
+  function createEvent(eventType, eventData){
+    var event = {};
+
+    event.time = Number(new Date());
+    event.type = eventType;
+    event.data = eventData;
+
+    return event;
+  };
+
+  function registerEventHandlers(){
+    // handle DOM Mutation events
+    // handle mouse movements
+    // handle keyboard events
+    // handle text selection events
+    // handle form submition event
+    // handle navigation event i.e. unloading of page
+
+    // TODO: Investigate how to record mouseover event for
+    // elements which use :hover, :active pseudo-elements
+    // may be querying for styles on mouseenter and 
+    // checking for changes in computed style
+    // would be the solution
+
+    // TODO: handle video playback events for youtube and vimeo
+  };
+
+  var screenjs = {};
+
+  screenjs.record = function(options){
+    options = options || {};
+    options.eventCallback = options.eventCallback || function(event){
+      console.log(event);
     };
 
-    console.log(getPageHTML());
+    var events = [];
 
-    var screen = {};
-
-    screen.record = function(options){
-      options.eventCallback ||= function(event){
-        console.log(event);
-      };
-
-      
+    var eventType = "start";
+    var eventData = {
+      html: getPageHTMLWithCSS(),
+      height: $(window).height(),
+      width: $(window).height()
     };
-  });
+
+    events.push(createEvent(eventType, eventData));
+
+    console.log(events);
+  };
+
+  window.screenjs = screenjs;
+
 })(window.screenjs$);
