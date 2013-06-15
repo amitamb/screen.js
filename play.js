a59884fc034d7de5d562902f2dfd00dcdf2785dc
Amit Ambardekar <amitamb@gmail.com>
2013-06-16 02:47:25 +0530

    DOM playback added as well

--- play.js
+++ play.js
@@ -7,17 +7,46 @@
 (function($){
   var screenjs = {};
 
-  screenjs.mouseCursor = $("<img></img>");
-  screenjs.mouseCursor.attr("src", "mouse4.png").css({
-    position:"absolute",
-    display:"block",
-    left:"-500px",
-    top:"-500px"
-  });
+  screenjs.base = "";
 
-  // page will be already loaded so no need to wait for page load
-  // as play.js is added after some HTML is written to page
-  screenjs.mouseCursor.appendTo("body");
+  function showMouseImages(){
+    screenjs.clickCircle = $("<img></img>");
+    screenjs.clickCircle.attr("src", "circle.png").css({
+      position:"absolute",
+      display:"block",
+      left:"-500px",
+      top:"-500px",
+      "z-index":"9999"
+    }).appendTo("body");
+    // page will be already loaded so no need to wait for page load
+    // as play.js is added after some HTML is written to page
+
+    screenjs.mouseCursor = $("<img></img>");
+    screenjs.mouseCursor.attr("src", "mouse4.png").css({
+      position:"absolute",
+      display:"block",
+      left:"-500px",
+      top:"-500px",
+      "z-index":"10000"
+    }).appendTo("body");
+  }
+
+  screenjs.mirror = new TreeMirror(document, {
+    createElement: function(tagName) {
+      if (tagName == 'SCRIPT') {
+        var node = document.createElement('NO-SCRIPT');
+        node.style.display = 'none';
+        return node;
+      }
+
+      if (tagName == 'HEAD') {
+        var node = document.createElement('HEAD');
+        node.appendChild(document.createElement('BASE'));
+        node.firstChild.href = screenjs.base;
+        return node;
+      }
+    }
+  });
 
   screenjs.setMousePosition = function(left, top){
     // screenjs.mouseCursor.
@@ -26,6 +55,49 @@
       top: top
     });
   };
+
+  screenjs.setMouseStatus = function(left, top, mouseDown){
+    if (mouseDown) {
+      screenjs.clickCircle.css({
+        left: left - 16,
+        top: top - 16,
+        display: "block"
+      });
+    }
+    else {
+      screenjs.clickCircle.css({
+        left: "-500px",
+        top: "-500px",
+        display: "none"
+      });
+    }
+  };
+
+  function clearPage() {
+    while (document.firstChild) {
+      document.removeChild(document.firstChild);
+    }
+  }
+
+  function handleMessage(msg) {
+    if (msg.clear)
+      clearPage();
+    else if (msg.base)
+      screenjs.base = msg.base;
+    else
+      screenjs.mirror[msg.f].apply(screenjs.mirror, msg.args);
+  }
+
+  screenjs.handleDOMMutation = function(data){
+    screenjs.mirror
+    console.log(data);
+    data.args = JSON.parse(data.args);
+    handleMessage(data);
+  };
+
+  clearPage();
+
+  showMouseImages();
 
   window.screenjs = screenjs;
 })(window.screenjs$);
--- screen.js
+++ screen.js
@@ -17,17 +17,6 @@
     return screenjs.playFrame.contentWindow.screenjs;
   };
 
-  screenjs.mouseCursor = $("<img></img>");
-  screenjs.mouseCursor.attr("src", "mouse4.png").css({
-    position:"absolute",
-    display:"block",
-    left:"-500px",
-    top:"-500px"
-  });
-  $(function(){
-    screenjs.mouseCursor.appendTo("body");
-  });
-
   function loadScriptInPlayFrame(filename){
     var frameDocument = screenjs.playFrame.contentDocument;
     var scriptRef = frameDocument.createElement('script');
@@ -51,6 +40,28 @@
     return htmlElem.html();
   };
 
+  function showMouseImages(){
+    screenjs.clickCircle = $("<img></img>");
+    screenjs.clickCircle.attr("src", "circle.png").css({
+      position:"absolute",
+      display:"block",
+      left:"-500px",
+      top:"-500px",
+      "z-index":"9999"
+    }).appendTo(screenjs.frameOverlay);
+    // page will be already loaded so no need to wait for page load
+    // as play.js is added after some HTML is written to page
+
+    screenjs.mouseCursor = $("<img></img>");
+    screenjs.mouseCursor.attr("src", "mouse4.png").css({
+      position:"absolute",
+      display:"block",
+      left:"-500px",
+      top:"-500px",
+      "z-index":"10000"
+    }).appendTo(screenjs.frameOverlay);
+  }
+
   function createEvent(eventType, eventData){
     var event = {};
 
@@ -70,35 +81,47 @@
   };
 
   function checkEventType(event, expectedType){
-    return event.type == expectedType;
+    if (expectedType instanceof Array) {
+      return expectedType.indexOf(event.type) >= 0;
+    }
+    else {
+      return event.type == expectedType;
+    }
   };
 
-  function getNextEvent(){
-    screenjs.currentEventIndex++;
+  function getNextEvent(dontIncrement){
+    if (!dontIncrement) {
+      screenjs.currentEventIndex++;
+    }
     return screenjs.events[screenjs.currentEventIndex];
   };
 
   var recordDOMMutationEvents = function(){
-    var observer = new MutationSummary({
-      callback: function(summaries){
-        console.log(summaries);
-        for(var i=0,len=summaries.length;i<len;i++){
-          var summary=summaries[i];
-          if (summary.added.length > 0) {
-            // element added
-            
-          }
-        }
+
+    var mirrorClient = new TreeMirrorClient(document, {
+      initialize: function(rootId, children) {
+        appendEvent("DOMMutation", {
+          f: 'initialize',
+          args: JSON.stringify([rootId, children])
+        });
       },
-      queries: [{ 
-        element: "*"
-      },
-      {
-        attribute:"style"
-      }]
+
+      applyChanged: function(removed, addedOrMoved, attributes, text) {
+        appendEvent("DOMMutation", {
+          f: 'applyChanged',
+          args: JSON.stringify([removed, addedOrMoved, attributes, text])
+        })
+        console.log(JSON.stringify([removed, addedOrMoved, attributes, text]));
+      }
     });
+
   };
-  var playDOMMutationEvents = function(){};
+  var playDOMMutationEvents = function(event){
+    if ( checkEventType(event, "DOMMutation") ) {
+      var eventData = event.data;
+      getPlayFrameScreenjs().handleDOMMutation(eventData);
+    }
+  };
 
   var recordMouseEvents = function(){
     // mouse movements
@@ -142,10 +165,41 @@
       // TODO: change position of mouse image on page
       var eventData = event.data;
       // TODO: Make sure it works in IE
-      getPlayFrameScreenjs().setMousePosition(eventData.pageX, eventData.pageY);
+      screenjs.mouseCursor.css({
+        left: eventData.pageX,
+        top: eventData.pageY
+      });
+      if (screenjs.isMouseDown){
+        screenjs.clickCircle.css({
+          left: eventData.pageX - 16,
+          top: eventData.pageY - 16,
+          display: "block"
+        });
+      }
+      // getPlayFrameScreenjs().setMousePosition(eventData.pageX, eventData.pageY);
     }
-    // TODO: Show mousedown event
-    // TODO: Show mouseup event
+    else if ( checkEventType(event, "mousedown") ) {
+      console.log("mousedown found");
+      screenjs.isMouseDown = true;
+      var eventData = event.data;
+      screenjs.clickCircle.css({
+        left: eventData.pageX - 16,
+        top: eventData.pageY - 16,
+        display: "block"
+      });
+      // getPlayFrameScreenjs().setMouseStatus(eventData.pageX, eventData.pageY, true);
+    }
+    else if ( checkEventType(event, "mouseup") ) {
+      console.log("mouseup found");
+      screenjs.isMouseDown = false
+      var eventData = event.data;
+      screenjs.clickCircle.css({
+        left: "-500px",
+        top: "-500px",
+        display: "none"
+      });
+      // getPlayFrameScreenjs().setMouseStatus(eventData.pageX, eventData.pageY, false);
+    }
   };
 
   var recordKeyboardEvents = function(){
@@ -217,6 +271,7 @@
     var eventType = "start";
     var eventData = {
       html: getPageHTMLWithCSS(),
+      base: window.location.href.match(/^(.*\/)[^\/]*$/)[1],
       height: $(window).height(),
       width: $(window).width()
     };
@@ -237,9 +292,13 @@
   };
 
   screenjs.playEvent = function(event){
-    if ( checkEventType(event, "mousemove") ) {
+    if ( checkEventType(event, ["mousemove", "mousedown", "mouseup"]) ) {
       playMouseEvents(event);
     }
+    else if ( checkEventType(event, "DOMMutation") ) {
+      playDOMMutationEvents(event);
+    }
+    return event.time;
   };
 
   screenjs.startEventLoop = function(){
@@ -248,30 +307,73 @@
     var $playFrame = $(screenjs.playFrame);
     screenjs.playFrame.left = 0;
     screenjs.playFrame.top = 0;
-    screenjs.intervalId = setInterval(function(){
-      // console.log("Inside interval");
-      var curTime = Number(new Date());
-      var curTimeOffset = curTime - screenjs.startPlayTime;
-      var event = null;
-      while ( event = getNextEvent() ){
-        var eventOffset = event.time - screenjs.startEventTime;
-        screenjs.playEvent(event);
-        if ( eventOffset >= curTimeOffset ) {
-          break;
-        }
+
+    // screenjs.intervalId = setInterval(function(){
+    //   // console.log("Inside interval");
+    //   var curTime = Number(new Date());
+    //   var curTimeOffset = curTime - screenjs.startPlayTime;
+    //   var event = null;
+    //   while ( event = getNextEvent() ){
+    //     var eventOffset = event.time - screenjs.startEventTime;
+    //     screenjs.playEvent(event);
+    //     if ( eventOffset >= curTimeOffset ) {
+    //       break;
+    //     }
+    //   }
+    //   if (!event) {
+    //     screenjs.stopPlaying();
+    //   }
+    // }, interval);
+
+    function playNextEvent(eventIndex){
+      var event = screenjs.events[eventIndex];
+      var nextEvent = screenjs.events[eventIndex+1];
+
+      if (nextEvent && screenjs.playing){
+        var timeDiff = nextEvent.time - event.time;
+        screenjs.timeoutId = setTimeout(function(){
+          screenjs.playEvent(event);
+          playNextEvent(eventIndex+1);
+        }, timeDiff);
       }
-      if (!event) {
-        screenjs.stopPlaying();
-      }
-    }, interval);
+    };
+
+    playNextEvent(0);
+    
     console.log("Interval created ID: " + screenjs.intervalId);
   };
 
   screenjs.play = function(options){
 
-    screenjs.playFrame = options.playFrame;
+    screenjs.playerContainer = options.playerContainer;
+
+    $playerContainer = $(playerContainer);
+
+    $playerContainer.html("<div><iframe></iframe></div>");
+
+    // TODO: Use class for this
+    screenjs.frameOverlay = $playerContainer.find("div").css({
+      width:"100%",
+      height:"100%",
+      overflow:"hidden",
+      position:"relative"
+    })[0];
+
+    showMouseImages();
+
+    // TODO: Use class for this
+    $playerContainer.find("iframe").css({
+      width:"100%",
+      height:"100%",
+      border:"0"
+    });
+
+    screenjs.playFrame = $playerContainer.find("iframe")[0];
     screenjs.events = options.events;
     screenjs.currentEventIndex = -1;
+
+    // TODO: Do initialization in seperate function
+    screenjs.isMouseDown = false;
 
     var event = getNextEvent();
     var eventData = screenjs.events[0].data;
@@ -286,27 +388,34 @@
 
     screenjs.playFrame.src = "about:blank";
     setTimeout(function(){
-      screenjs.playFrame.contentDocument.write(eventData.html);
+      //screenjs.playFrame.contentDocument.write(eventData.html);
+
       loadScriptInPlayFrame("http://code.jquery.com/jquery-1.9.1.min.js");
       loadScriptInPlayFrame("init.js");
-      loadScriptInPlayFrame("play.js");
+      loadScriptInPlayFrame("mutation_summary.js");
+      loadScriptInPlayFrame("tree_mirror.js");
+      loadScriptInPlayFrame("play.js?a3");
 
       // TODO: Wait for play.js to load and then continue
       // imnprove it from simple setTimeout
       setTimeout(function(){
+
+        var t = eventData.base;
+        getPlayFrameScreenjs().base = eventData.base
+
         screenjs.startPlayTime = Number(new Date());
         screenjs.startEventTime = event.time;
         screenjs.playing = true;
 
         screenjs.startEventLoop();
-      }, 20);
+      }, 300);
     }, 10)
   };
 
   screenjs.stopPlaying = function(){
     screenjs.playing = false;
     console.log("Interval cleared : " + screenjs.intervalId);
-    clearInterval(screenjs.intervalId);
+    clearInterval(screenjs.timeoutId);
   };
 
   window.screenjs = screenjs;
